95a5fae88ee3645cd677618a4ce67e2da8debac1
Valéria <valeriaoli2003@gmail.com>
2024-10-26 15:21:59 +0000

    Adicionando

---
+++ demo/src/main/java/com/example/demo/ProfessorRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfessorRepo extends CrudRepository<Professor, Long> {
+	Iterable<Professor> findByDisciplinaId(int DisciplinaId);
+}
